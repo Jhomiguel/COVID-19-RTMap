1ec6682f450851cc052a348227cb732361133c9a
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-03-27 22:44:15 -0400

    Adding COVID-19 Global Locations from an API

--- src/App.js
+++ src/App.js
@@ -1,127 +1,126 @@
-import React,{useState,useEffect} from 'react';
-import './App.css';
-import Header from './components/Header'
-import CovidMap from './components/CovidMap'
-import firebase from 'firebase'
-import AddCase from './components/AddCase'
-
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import CovidMap from "./components/CovidMap";
+import firebase from "firebase";
+import AddCase from "./components/AddCase";
 
 function App() {
-  
-  const[datos,guardarDatos]=useState({user:''}); 
-  const[posicion,guardarPosicionActual]=useState({posicionactual: null});
-  const[posiciones,guardarPosiciones]=useState({ubicaciones: []})
-  const[userposition,saveUserCurrentPosition]=useState({lat:null,lng:null,zoom:null})
-  
-  const {user}=datos;
+  const [datos, guardarDatos] = useState({ user: "" });
+  const [posicion, guardarPosicionActual] = useState({ posicionactual: null });
+  const [posiciones, guardarPosiciones] = useState({ ubicaciones: [] });
+  const [userposition, saveUserCurrentPosition] = useState({
+    lat: 16.501429,
+    lng: -71.493585,
+    zoom: 5
+  });
+  const [apidata, saveApiData] = useState([]);
 
-  // latlng={
-  //   lng:location.coords.latitude,
-  //   lat:location.coords.longitude
-    
-      const handleCurrentPosition = ()=>{
-        if(user){
-          navigator.geolocation.getCurrentPosition(location=>(
-            saveUserCurrentPosition({
-            lat:location.coords.latitude.toPrecision(14),
-            lng: location.coords.longitude.toPrecision(14),
-          zoom:10})
-                  
-          ));
+  const Axios = require("axios");
+  useEffect(() => {
+    Axios.get("https://covid19.mathdro.id/api/confirmed")
+      .then(res => {
+        saveApiData(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const { user } = datos;
+
+  const handleCurrentPosition = () => {
+    if (user) {
+      navigator.geolocation.getCurrentPosition(location =>
+        saveUserCurrentPosition({
+          lat: location.coords.latitude.toPrecision(14),
+          lng: location.coords.longitude.toPrecision(14),
+          zoom: 3
+        })
+      );
+    }
+  };
+
+  const handlePosition = e => {
+    guardarPosicionActual({ posicionactual: e.latlng });
+  };
+
+  useEffect(
+    () =>
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          guardarDatos({ user: user });
+          handleCurrentPosition();
+        } else {
+          guardarDatos({ user: null });
         }
-        else{
-          saveUserCurrentPosition({
-            lat:null,
-            lng:null
-          })
-        }
-      }
+      }),
+    [datos]
+  );
 
+  useEffect(
+    () =>
+      firebase
+        .database()
+        .ref("VirusMapLocations")
+        .on("child_added", snapshot => {
+          guardarPosiciones({
+            ubicaciones: posiciones.ubicaciones.concat(snapshot.val())
+          });
+        }),
+    // eslint-disable-next-line
+    []
+  );
 
+  const handleAuth = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => console.log(result.user.email));
+  };
+  const handleLogOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("Te has desconectado"))
+      .catch(error => console.error(`Eror: ${error.code}: ${error.message}`));
+  };
 
-      const handlePosition=e=>{
-        guardarPosicionActual({posicionactual: e.latlng});
-      }
-      
-      useEffect(()=>(
-        firebase.auth().onAuthStateChanged(user=>{
-          if(user){
-            guardarDatos({user: user})
-          }else{
-            guardarDatos({user:null})
-          }
-        })),[datos])
+  const handleCase = () => {
+    const dbRef = firebase.database().ref("VirusMapLocations");
+    const newLocation = dbRef.push();
+    const data = {
+      latlng: {
+        lng: posicion.posicionactual.lng,
+        lat: posicion.posicionactual.lat
+      },
+      CasoRegistradoPor: datos.user.displayName
+    };
+    newLocation.set(data);
+    alert("Se ha registrado un nuevo caso");
+  };
 
-        useEffect(()=>(
-          firebase.database().ref('VirusMapLocations').on('child_added',snapshot=>{
-            guardarPosiciones({
-                
-                ubicaciones: posiciones.ubicaciones.concat(snapshot.val())
-            })
-          })
-          // eslint-disable-next-line
-        ),[])
-        
-        const handleAuth=()=>{
-          const provider= new firebase.auth.GoogleAuthProvider();
-          firebase.auth().signInWithPopup(provider)
-          .then(result=>console.log(result.user.email))
-         }
-        const handleLogOut=()=>{
-          firebase.auth().signOut()
-          .then(()=>console.log('Te has desconectado'))
-          .catch(error=>console.error(`Eror: ${error.code}: ${error.message}`))
-        }
-
-        const handleCase=()=>{
-          
-            const dbRef=firebase.database().ref('VirusMapLocations')
-            const newLocation = dbRef.push();
-            const data={
-              latlng:{
-              lng:posicion.posicionactual.lng,
-              lat:posicion.posicionactual.lat
-              },
-              CasoRegistradoPor: datos.user.displayName
-            }
-            newLocation.set(data);
-            alert('Se ha registrado un nuevo caso')
-            
-        }
-
-     return (
-      
-      <div className="contenido">
-        <h1 className="header">Covid-19 Map Tracker</h1>
+  return (
+    <div className="contenido">
+      <h1 className="header">Covid-19 Map Tracker</h1>
       <div className="contenido-principal">
-        <button onClick={handleCurrentPosition}>GetPosition</button>
-      <Header
-        user={datos.user}
-        handleAuth={handleAuth}
-        handleLogOut={handleLogOut}/>
-      <CovidMap 
-        handlePosition={handlePosition}
-        user={datos.user}
-        posicion={posicion}
-        posiciones={posiciones.ubicaciones}
-        userCurrentPosition={userposition}
-      />
-      
-       {user?(
-        <AddCase
-        posicion={posicion}
-        handleCase={handleCase}
+        <Header
+          user={datos.user}
+          handleAuth={handleAuth}
+          handleLogOut={handleLogOut}
         />
-        
-       ):
-       (
-        null
-       )}
-     
+        <CovidMap
+          handlePosition={handlePosition}
+          user={datos.user}
+          posicion={posicion}
+          posiciones={posiciones.ubicaciones}
+          userCurrentPosition={userposition}
+          apiData={apidata}
+          saveApiData={saveApiData}
+        />
+
+        {user ? <AddCase posicion={posicion} handleCase={handleCase} /> : null}
       </div>
-      
-      </div>
+    </div>
   );
 }
 
--- src/components/CovidMap.js
+++ src/components/CovidMap.js
@@ -1,63 +1,59 @@
-import React from 'react';
-import {Map,Marker,Popup,TileLayer} from 'react-leaflet'
+import React from "react";
+import { Map, Marker, Popup, TileLayer, L } from "react-leaflet";
 
+const CovidMap = ({
+  handlePosition,
+  user,
+  posicion,
+  ubicaciones,
+  userCurrentPosition,
+  apiData,
+  saveApiData
+}) => {
+  const { lat, lng, zoom } = userCurrentPosition;
 
-// eslint-disable-next-line
-const consultarApi=()=>{
-    const api =fetch('https://covid19.mathdro.id/api/confirmed');
-    const resultado= api.then(Response=> Response.json())
-    // eslint-disable-next-line
-    const res=  resultado.then(function(dato){
-        return;
-    })  
-}
+  return (
+    <Map center={[lat, lng]} zoom={zoom} onClick={handlePosition}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
 
- 
+      {apiData.slice(0 - 80).map(datos => {
+        return (
+          <Marker position={[datos.lat, datos.long]} draggable={true}>
+            <Popup position={saveApiData}>
+              <div>
+                Place: {datos.combinedKey} <br />
+                Infected:{datos.confirmed} <br />
+                Recovered:{datos.recovered} <br />
+                Deaths:{datos.deaths} <br />
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
 
-const CovidMap = ({handlePosition,user,posicion,ubicaciones,userCurrentPosition}) => {
+      {user ? (
+        <Marker position={{ lat, lng }} draggable={true}>
+          <Popup position={{ lat, lng }}>
+            Mi ubicacion actual es:{" "}
+            <pre>{JSON.stringify({ lat, lng }, null, 2)}</pre>
+          </Popup>
+        </Marker>
+      ) : null}
+      {user
+        ? posicion.posicionactual && (
+            <Marker position={posicion.posicionactual} draggable={true}>
+              <Popup position={posicion.posicionactual}>
+                Ubicacion del marcador{" "}
+                <pre>{JSON.stringify(posicion, null, 2)}</pre>
+              </Popup>
+            </Marker>
+          )
+        : (posicion.posicionactual = null)}
+    </Map>
+  );
+};
 
-    const {lat,lng,zoom}= userCurrentPosition
-     {/* {ubicaciones.map(location=>(
-                    <Marker position={location.ubicacion} draggable={true}>
-                      <Popup position={location.ubicacion}>
-                          Ubicacion: <pre>{JSON.stringify(location, null, 2)}</pre>
-                      </Popup>
-                      </Marker>
-                  ))  
-                  } */}
-                    {/* <Marker position={ubicaciones.latlng} draggable={true}>
-                      </Marker> */}
-
-  
-    return ( 
-     
-        <Map center={[lat, lng]} zoom={zoom} onClick={handlePosition}>
-           <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  />
-                      {user?(
-                        <Marker position={{lat,lng}} draggable={true}>
-                        <Popup position={{lat,lng}}>
-                           Mi ubicacion actual es:  <pre>{JSON.stringify({lat,lng}, null, 2)}</pre>
-                        </Popup>
-                        </Marker>
-                      ):(
-                        null
-                      )}
-                  
-                  
-                    {
-                      user
-                      ? (posicion.posicionactual && <Marker position={posicion.posicionactual} draggable={true}>
-                      <Popup position={posicion.posicionactual}>
-                          Ubicacion del marcador <pre>{JSON.stringify(posicion, null, 2)}</pre>
-                      </Popup>
-                      </Marker>)
-                      :(posicion.posicionactual= null)
-                    }
-        </Map>
-     );
-}
- 
 export default CovidMap;
